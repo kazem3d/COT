6d5646e66495e462842e191a2ab581680133afa4
kazem <kazem3d@gmail.com>
2020-06-04 15:37:07 +0430

    init

---
+++ web/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Currency,CotData
+
+# Register your models here.
+class CotDataAdmin(admin.ModelAdmin):
+    list_display = ('name','date','long','short','long_change',
+                    'short_change','long_percent','short_percent','net_positions')
+    list_filter = ('name',)
+    ordering = ['date']
+
+admin.site.register(Currency)
+admin.site.register(CotData,CotDataAdmin)
+
---
+++ web/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class Currency(models.Model):
+    name=models.CharField(max_length=3)
+
+    class Meta:
+            verbose_name = 'currency'
+            verbose_name_plural = 'currencies'
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class CotData (models.Model):
+
+    name=models.ForeignKey('Currency',on_delete=models.CASCADE)
+    date=models.DateField()
+    long=models.IntegerField()
+    short=models.IntegerField()
+    long_change=models.IntegerField()
+    short_change=models.IntegerField()
+    long_percent=models.IntegerField()
+    short_percent=models.IntegerField()
+
+    class Meta:
+            verbose_name = 'cot data'
+            verbose_name_plural = 'cot data'
+
+    def net_positions(self):
+        return self.long - self.short
+
+    def __str__(self):
+        return '{}    {}'.format(self.name,self.date)
+
