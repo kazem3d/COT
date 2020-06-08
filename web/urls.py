041145f0b06cf216e6831751838188dceb01c88e
Ghanati-PC <Ghanati-PC@Ghanati-PC.arak.com>
2020-06-08 14:09:06 +0430

    working on currency

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CotListView,currency_list
+from .views import CotListView,currency_list,import_data
 
 app_name='web'
 urlpatterns = [
 
     path('',CotListView.as_view(),name='home'),
     path('<slug:slug>/',currency_list,name='currency'),
-
+    path('q/import/',import_data,name='import_data'),
 
         ]
--- web/views.py
+++ web/views.py
@@ -21,3 +21,28 @@ def currency_list(request,slug):
         'pairs':pairs,
     }
     return render(request,'web/currency.html',context)
+
+def import_data(request):
+    import sqlite3
+
+    conn=sqlite3.connect('report_database')
+    curser=conn.cursor()
+
+    #function for reading a table from date base and return data of row in list of list
+    def read_from_datebase(table_name):
+        _list=[]
+        curser.execute('SELECT currency,date,long,short,long_change,short_change,long_change_percent,short_change_percent FROM %s ' %table_name)
+        rows=curser.fetchall()
+        for row in rows:
+            row=list(row)
+            _list.append(row)
+        return _list
+
+    row1=read_from_datebase('main')[1]
+
+    print("*****************")
+    print(row1)
+    # obj=CotData(name=row1[0],date=row1[1],long=row1[2])
+    # obj.save()
+
+    return HttpResponse('import done')
