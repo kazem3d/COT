92712906fd197e183663f0a88fd0cf9bd23f63dc
kazem <kazem3d@gmail.com>
2020-06-19 00:11:07 +0430

    making upload page

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CotListView,currency_list,import_data
+from .views import CotListView,currency_list,import_data,upload
 
 app_name='web'
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('',CotListView,name='home'),
     path('<slug:slug>/',currency_list,name='currency'),
     path('q/import/',import_data,name='import_data'),
+    path('q/upload/',upload,name='upload'),
+
 
         ]
--- web/views.py
+++ web/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, Http404
 from .models import  CotData,Currency
 from django.views.generic import ListView 
 from django.contrib.auth.decorators import login_required
+import sqlite3
+from django.core.files.storage import FileSystemStorage
+
 
 # Create your views here.
 
@@ -111,3 +114,41 @@ def import_data(request):
         obj.save()
 
     return HttpResponse('import done')
+
+def upload(request):
+
+    #function for reading a table from date base and return data of row in list of list
+    def read_from_datebase(table_name):
+        _list=[]
+        curser.execute('SELECT currency,date,long,short,long_change,short_change,long_change_percent,short_change_percent FROM %s ' %table_name)
+        rows=curser.fetchall()
+        for row in rows:
+            row=list(row)
+            _list.append(row)
+        return _list
+
+
+
+    if request.method == "POST":
+        upload_file=request.FILES['myfile']
+        fs = FileSystemStorage()
+        fs.delete('report_database')
+        filename = fs.save('report_database', upload_file)
+       
+        conn=sqlite3.connect('media/report_database')
+        curser=conn.cursor()
+        
+        database_list=read_from_datebase('main')
+        for row in database_list:
+
+            print("*****************")
+            print(row)
+            currency_name=Currency.objects.get(name=row[0])
+
+            obj=CotData(name=currency_name,date=row[1],long=row[2],short=row[3],
+                            long_change=row[4],short_change=row[5],long_percent=row[6],
+                            short_percent=row[7])
+            obj.save()
+        
+
+    return render (request,'web/upload.html')
